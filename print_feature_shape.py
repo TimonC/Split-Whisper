09c99d9b5fc2a48317a9f443bf9a9d311b20496e
TCrouzen <timon.crouzen@gmail.com>
2025-05-29 22:16:33 +0200

    print all spontaneous

--- print_feature_shape.py
+++ print_feature_shape.py
@@ -3,4 +3,6 @@ import numpy as np
 data = load_data_custom_cslu("data_cslu_splits/all/data/spontaneous/all_ages_all_genders", mode="test")
 print(data)
 test_data = data['test']
-print(np.array(test_data[0]['input_features']).shape)
+for entry_idx in range(len(test_data)):
+    print(np.array(test_data[entry_idx]['input_features']).shape)
+print("done printing feature shapes of all spontaneaus data entries")
