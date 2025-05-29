c18200e405e7f8af619ba3e0b513fcbc4865d87b
TCrouzen <timon.crouzen@gmail.com>
2025-05-29 14:37:53 +0200

    fix data path input

--- transcribe_custom_cslu.py
+++ transcribe_custom_cslu.py
@@ -36,12 +36,10 @@ def transcribe(args):
         chunk_length_s=30,
     )
 
-    # Prepare test data path
-    test_path = os.path.join(args.data_path, args.json_option, "data", args.cslu_option, "test")
-    print(f"Test dataset: {test_path}")
-
-    # Load test data
-    testsets = load_data_custom_cslu(test_path, mode = "test")
+    # Load in test data
+    data_path = os.path.join(args.data_path, args.json_option, "data", args.cslu_option)
+    print(f"Dataset: {data_path}")
+    testsets = load_data_custom_cslu(data_path, mode = "test")
 
     # Prepare output directory
     transcription_dir = os.path.join("huggingface_models_transcription", finetuned_model, "transcriptions")
