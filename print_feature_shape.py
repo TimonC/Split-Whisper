314bf89c62037ca57906cf59e0dcddd30b477e3b
TCrouzen <timon.crouzen@gmail.com>
2025-05-29 22:13:06 +0200

    print change

--- print_feature_shape.py
+++ print_feature_shape.py
@@ -2,6 +2,4 @@ from load_data_custom_cslu import load_data_custom_cslu
 data = load_data_custom_cslu("data_cslu_splits/all/data/spontaneous/all_ages_all_genders", mode="test")
 print(data)
 test_data = data['test']
-print(test_data)
-feats = test_data['input_features']
-print(feats[0].shape) 
+print(test_data[0])
