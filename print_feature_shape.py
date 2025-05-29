b8d95fe088c536bd650664ecb5b02f053031cfd2
TCrouzen <timon.crouzen@gmail.com>
2025-05-29 22:06:59 +0200

    gonnak eep changing prints

--- print_feature_shape.py
+++ print_feature_shape.py
@@ -1,4 +1,5 @@
 from load_data_custom_cslu import load_data_custom_cslu
 data = load_data_custom_cslu("data_cslu_splits/all/data/spontaneous/all_ages_all_genders", mode="test")
 print(data)
-print(data['test']['input_features'][0])
+test_data = data['test']
+print(test_data)
