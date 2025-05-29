1d70e1f842cd2273ed42e5cf4bb24d84478f32ae
TCrouzen <timon.crouzen@gmail.com>
2025-05-29 22:02:47 +0200

    add shape print

--- print_feature_shape.py
+++ print_feature_shape.py
@@ -1,3 +1,4 @@
 from load_data_custom_cslu import load_data_custom_cslu
 data = load_data_custom_cslu("data_cslu_splits/all/data/spontaneous/all_ages_all_genders", mode="test")
 print(data)
+print(data['test']['input_features'][0].shape)
